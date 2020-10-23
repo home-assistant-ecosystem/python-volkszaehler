1f851948c1e869762f0e72be7233dd3edb675833
bbr111 <46243805+bbr111@users.noreply.github.com>
2020-10-23 11:38:30 +0200

    Update __init__.py
    Get Last Value with parameter from now .

--- volkszaehler/__init__.py
+++ volkszaehler/__init__.py
@@ -9,7 +9,7 @@ from . import exceptions
 
 _LOGGER = logging.getLogger(__name__)
 _RESOURCE = "http://{host}:{port}/middleware.php/data/{uuid}.json"
-
+_RESOURCE1 = "http://{host}:{port}/middleware.php/data/{uuid}.json?from=now"
 
 class Volkszaehler(object):
     """A class for handling the data retrieval."""
@@ -19,8 +19,10 @@ class Volkszaehler(object):
         self._loop = loop
         self._session = session
         self.url = _RESOURCE.format(host=host, port=port, uuid=uuid)
+        self.url1 = _RESOURCE1.format(host=host, port=port, uuid=uuid)
         self.data = {}
-        self.average = self.max = self.min = self.consumption = None
+        self.data1 = {}
+        self.average = self.max = self.min = self.consumption = self.last = None
         self.tuples = []
 
     async def get_data(self):
@@ -28,13 +30,16 @@ class Volkszaehler(object):
         try:
             with async_timeout.timeout(5, loop=self._loop):
                 response = await self._session.get(self.url)
+                response1 = await self._session.get(self.url1)
 
             _LOGGER.debug("Response from Volkszaehler API: %s", response.status)
             self.data = await response.json()
+            self.data1 = await response.json()
             _LOGGER.debug(self.data)
         except (asyncio.TimeoutError, aiohttp.ClientError):
             _LOGGER.error("Can not load data from Volkszaehler API")
             self.data = None
+            self.data1 = None
             raise exceptions.VolkszaehlerApiConnectionError()
 
         self.average = self.data["data"]["average"]
@@ -42,3 +47,4 @@ class Volkszaehler(object):
         self.min = self.data["data"]["min"][1]
         self.consumption = self.data["data"]["consumption"]
         self.tuples = self.data["data"]["tuples"]
+        self.last = self.data["data1"]["average"]
