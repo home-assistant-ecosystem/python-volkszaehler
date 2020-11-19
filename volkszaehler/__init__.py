5cbc53caa949e7e4dbaa0bebb04b91d6d4480280
Benjamin <46243805+bbr111@users.noreply.github.com>
2020-11-19 13:04:34 +0100

    Update __init__.py - Add Last Funktion with From/to
    Add Parameter for FROM and TO Queries
    Add self.last using tuples array

--- volkszaehler/__init__.py
+++ volkszaehler/__init__.py
@@ -9,36 +9,39 @@ from . import exceptions
 
 _LOGGER = logging.getLogger(__name__)
 _RESOURCE = "{schema}://{host}:{port}/middleware.php/data/{uuid}.json"
-_RESOURCENOW = "{schema}://{host}:{port}/middleware.php/data/{uuid}.json?from=now"
-
+_RESOURCE_FROM = "from={param_from}"
+_RESOURCE_TO = "to={param_to}"
 
 class Volkszaehler(object):
     """A class for handling the data retrieval."""
 
-    def __init__(self, loop, session, uuid, host="localhost", port=80, tls=False):
+    def __init__(self, loop, session, uuid, host="localhost", port=80, tls=False, param_from="today", param_to="now"):
         """Initialize the connection to the API."""
         self._loop = loop
         self._session = session
         if tls:
             self.url = _RESOURCE.format(schema="https", host=host, port=port, uuid=uuid)
-            self.urlnow = _RESOURCENOW.format(schema="https", host=host, port=port, uuid=uuid)
         else:
             self.url = _RESOURCE.format(schema="http", host=host, port=port, uuid=uuid)
-            self.urlnow = _RESOURCENOW.format(schema="http", host=host, port=port, uuid=uuid)
         self.data = {}
         self.average = self.max = self.min = self.consumption = None
         self.tuples = []
+
+        _PREFIX = "?"
+        if param_from:
+            self.url += _PREFIX + _RESOURCE_FROM.format(param_from=param_from)
+            _PREFIX = "&"
+        if param_to:
+            self.url += _PREFIX + _RESOURCE_TO.format(param_to=param_to)
 
     async def get_data(self):
         """Retrieve the data."""
         try:
             with async_timeout.timeout(5, loop=self._loop):
                 response = await self._session.get(self.url)
-                responsenow = await self._session.get(self.urlnow)
 
             _LOGGER.debug("Response from Volkszaehler API: %s", response.status)
             self.data = await response.json()
-            self.datanow = await responsenow.json()
             _LOGGER.debug(self.data)
         except (asyncio.TimeoutError, aiohttp.ClientError):
             _LOGGER.error("Can not load data from Volkszaehler API")
@@ -50,4 +53,4 @@ class Volkszaehler(object):
         self.min = self.data["data"]["min"][1]
         self.consumption = self.data["data"]["consumption"]
         self.tuples = self.data["data"]["tuples"]
-        self.last = self.datanow["data"]["average"]
+        self.last = self.data["data"]["tuples"][-1][1]
